0f8f2469f364600496d4a2f526c66fa4abbf696e
N-Endy <okafornel@gmail.com>
2024-07-26 15:00:58 +0100

    Add production json file

--- CompanyEmployees/obj/Debug/net8.0/CompanyEmployees.AssemblyInfo.cs
+++ CompanyEmployees/obj/Debug/net8.0/CompanyEmployees.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c864b5a3ecade61386bbd8f0a559f9a60bfcd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
