3872934508dfe011b6e2b633d0995d8ac8e76b5f
N-Endy <okafornel@gmail.com>
2024-07-31 12:02:41 +0100

    Migrate database

--- Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f477f8688239b0b0c21c5546404a6ae459fe2e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2a1f2cc6e2ffc4c2abe701071d7a7f9fbfce3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
