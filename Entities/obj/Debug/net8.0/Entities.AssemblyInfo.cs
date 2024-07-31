9507529b67b62f2030e52baffdc50af57186e9cf
N-Endy <okafornel@gmail.com>
2024-07-31 12:08:19 +0100

    Migrate data

--- Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2a1f2cc6e2ffc4c2abe701071d7a7f9fbfce3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3872934508dfe011b6e2b633d0995d8ac8e76b5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
