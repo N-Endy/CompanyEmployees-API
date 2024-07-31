e17eafd2885eee319b42013214f46a7cb311b0b7
N-Endy <okafornel@gmail.com>
2024-07-31 12:32:13 +0100

    Add Initial Data in database

--- LoggerService/obj/Debug/net8.0/LoggerService.AssemblyInfo.cs
+++ LoggerService/obj/Debug/net8.0/LoggerService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3872934508dfe011b6e2b633d0995d8ac8e76b5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9507529b67b62f2030e52baffdc50af57186e9cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
