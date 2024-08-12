becbc6365e38d4b02b0a4757500b395271249c5f
N-Endy <okafornel@gmail.com>
2024-08-12 22:14:40 +0100

    Use DTO for transferring data

--- LoggerService/obj/Debug/net8.0/LoggerService.AssemblyInfo.cs
+++ LoggerService/obj/Debug/net8.0/LoggerService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d7f9d1e2f2c29a202f4899f10da133a8454d511")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc285d947cfcc2637dac6cd7b9fd4bc28d0b5362")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Shared/DataTransferObjects/CompanyDto.cs
@@ -0,0 +1,7 @@
+namespace Shared.DataTransferObjects;
+public record CompanyDto
+(
+    Guid Id, 
+    string Name, 
+    string FullAddress
+);
